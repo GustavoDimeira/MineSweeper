7241d1edce1c81f4b111451c4ee81fae8d9f203e
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-12-29 15:20:09 -0400

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Iniciando portifolio
         </a>
       </header>
     </div>
